ee298aafae4416ded39b338ae0dabdfe99cb5af9
Jake <jacob.b.singer@gmail.com>
2021-05-19 06:02:51 -0400

    adding netlify cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import { Link } from "gatsby"
 import * as React from "react"
+import { Fragment } from "react"
+import { Helmet } from "react-helmet"
 
 /* This example requires Tailwind CSS v2.0+ */
 const navigation = [
@@ -12,57 +14,65 @@ const navigation = [
 
 export default function Example() {
   return (
-    <header className="bg-indigo-600">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
-          <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
-              <img
-                className="h-10 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-                alt=""
-              />
-            </a>
-            <div className="hidden ml-10 space-x-8 lg:block">
-              {navigation.map(link => (
-                <Link
-                  key={link.name}
-                  to={link.href}
-                  className="text-base font-medium text-white hover:text-indigo-50"
-                >
-                  {link.name}
-                </Link>
-              ))}
+    <Fragment>
+      <Helmet>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Helmet>
+      <header className="bg-indigo-600">
+        <nav
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
+          aria-label="Top"
+        >
+          <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+            <div className="flex items-center">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-10 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                  alt=""
+                />
+              </a>
+              <div className="hidden ml-10 space-x-8 lg:block">
+                {navigation.map(link => (
+                  <Link
+                    key={link.name}
+                    to={link.href}
+                    className="text-base font-medium text-white hover:text-indigo-50"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+            <div className="ml-10 space-x-4">
+              <a
+                href="#"
+                className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+              >
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              >
+                Sign up
+              </a>
             </div>
           </div>
-          <div className="ml-10 space-x-4">
-            <a
-              href="#"
-              className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
-            >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
-            >
-              Sign up
-            </a>
+          <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+            {navigation.map(link => (
+              <a
+                key={link.name}
+                href={link.href}
+                className="text-base font-medium text-white hover:text-indigo-50"
+              >
+                {link.name}
+              </a>
+            ))}
           </div>
-        </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map(link => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-base font-medium text-white hover:text-indigo-50"
-            >
-              {link.name}
-            </a>
-          ))}
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </Fragment>
   )
 }
