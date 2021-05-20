b03e7d57f309c123df4be4184ab45115ef332b38
Jake <jacob.b.singer@gmail.com>
2021-05-20 14:25:42 -0400

    adding swapstack favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,15 @@ import React from "react"
 import { Fragment } from "react"
 import Header from "./header"
 import Footer from "./footer"
+import favicon from "../images/favicon.ico"
+import Helmet from "react-helmet"
 
 export default function Layout({ children }) {
   return (
     <Fragment>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Header />
       {children}
       <Footer />
