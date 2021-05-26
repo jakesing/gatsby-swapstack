d88c43dac304609a33142d7b6efa5f3458f46f37
Jake <jacob.b.singer@gmail.com>
2021-05-26 13:58:06 -0400

    fixing footer and header links

--- src/components/header.js
+++ src/components/header.js
@@ -409,13 +409,13 @@ export default function Header() {
                 {/* Sign up buttons */}
                 <div className="flex items-center md:ml-12">
                   <a
-                    href="#"
+                    href="https://app.swapstack.co/login?key=login"
                     className="text-base font-medium text-gray-500 hover:text-gray-900"
                   >
                     Sign in
                   </a>
                   <a
-                    href="#"
+                    href="https://app.swapstack.co/login"
                     className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                   >
                     Sign up
