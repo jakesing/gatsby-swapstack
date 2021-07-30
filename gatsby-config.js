89c2566b6c21647aa3cf413758be2e0bc9acceab
Jake <jacob.b.singer@gmail.com>
2021-07-30 13:20:42 -0400

    trying to fix gtag placement

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "UA-181112024-1", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
       },
     },
