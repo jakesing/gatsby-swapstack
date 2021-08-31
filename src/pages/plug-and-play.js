a6d9ba9e75f2af0793367ba82ccf38bd1fb1ed06
Jake <jacob.b.singer@gmail.com>
2021-08-31 18:40:18 -0400

    removing dead files

--- src/pages/plug-and-play.js
+++ src/pages/plug-and-play.js
@@ -2,10 +2,9 @@ import Layout from "../components/layout"
 /* This example requires Tailwind CSS v2.0+ */
 
 import React from "react"
-import FAQ from "../components/plug-and-play/faq"
 import Header from "../components/plug-and-play/header"
 import Features from "../components/plug-and-play/features"
-import CTA from "../components/plug-and-play/cta"
+
 import Seo from "../components/seo"
 
 export default function Writers() {
