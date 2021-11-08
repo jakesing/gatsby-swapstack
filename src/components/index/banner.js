a4ee526179d31ef35e05b40e6639c1ede968a8da
Jake <jacob.b.singer@gmail.com>
2021-11-07 21:17:21 -0500

    fixing mobile

--- src/components/index/banner.js
+++ src/components/index/banner.js
@@ -17,8 +17,9 @@ export default function Banner() {
             </span>
             <p className="ml-3 font-medium text-white truncate">
               <span className="md:hidden">No Fee November!</span>
-              Big news! Brands pay 0% fees from now until the end of November!
-              <span className="hidden md:inline"></span>
+              <span className="hidden md:inline">
+                Big news! Brands pay 0% fees from now until the end of November!
+              </span>
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
@@ -26,8 +27,14 @@ export default function Banner() {
               to="/introducing-no-fee-november/"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
-              Read the blog post!
+              Read more!
             </Link>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+            ></button>
           </div>
         </div>
       </div>
