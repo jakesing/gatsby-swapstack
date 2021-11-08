5aa11cc27ecbc6109786e00dd5aeb16e09a7f1d0
Jake <jacob.b.singer@gmail.com>
2021-11-07 20:55:46 -0500

    jake pic

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@
 import { Link } from "gatsby"
 import * as React from "react"
 import SwapstackLogo from "../images/swapstack_logo.png"
-
+import Banner from "./index/banner"
 import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import {
@@ -132,37 +132,38 @@ const navButtons = [
 
 export default function Header() {
   return (
-    <Popover className="relative bg-white">
-      {({ open }) => (
-        <>
-          <div
-            className="absolute inset-0 shadow z-30 pointer-events-none"
-            aria-hidden="true"
-          />
-          <div className="relative z-20">
-            <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-              <div>
-                <Link to="/" className="flex">
-                  <span className="sr-only">Workflow</span>
-                  <img
-                    className="h-6 w-auto sm:h-6"
-                    src={SwapstackLogo}
-                    alt="SwapstackLogo"
-                  />
-                </Link>
-              </div>
-              <div className="-mr-2 -my-2 md:hidden">
-                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                  <span className="sr-only">Open menu</span>
-                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
-              <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
-                {/* The Original Code for the flyover options - commenting and hiding the entire thing! */}
-                {/* Nav links */}
-                <Popover.Group as="nav" className="flex space-x-10">
-                  {/* Solutions */}
-                  {/* <Popover>
+    <div>
+      <Popover className="relative bg-white">
+        {({ open }) => (
+          <>
+            <div
+              className="absolute inset-0 shadow z-30 pointer-events-none"
+              aria-hidden="true"
+            />
+            <div className="relative z-20">
+              <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+                <div>
+                  <Link to="/" className="flex">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="h-6 w-auto sm:h-6"
+                      src={SwapstackLogo}
+                      alt="SwapstackLogo"
+                    />
+                  </Link>
+                </div>
+                <div className="-mr-2 -my-2 md:hidden">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Open menu</span>
+                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+                <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
+                  {/* The Original Code for the flyover options - commenting and hiding the entire thing! */}
+                  {/* Nav links */}
+                  <Popover.Group as="nav" className="flex space-x-10">
+                    {/* Solutions */}
+                    {/* <Popover>
                     {({ open }) => (
                       <>
                         <Popover.Button
@@ -401,108 +402,110 @@ export default function Header() {
                       </>
                     )}
                   </Popover> */}
-                  {navButtons.map(item => (
-                    <Link
-                      className="text-base font-medium text-gray-500 hover:text-gray-900"
-                      to={item.to}
-                      activeClassName="text-base font-bold text-gray-900 hover:text-gray-900"
-                      key={item.name}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </Popover.Group>
-                {/* Sign up buttons */}
-                <div className="flex items-center md:ml-12">
-                  <a
-                    href="https://app.swapstack.co/login?key=login"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Sign in
-                  </a>
-                  <a
-                    href="https://app.swapstack.co/login"
-                    className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                    Sign up
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="duration-200 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              static
-              className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-            >
-              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div className="pt-5 pb-6 px-5 sm:pb-8">
-                  <div className="flex items-center justify-between">
-                    <Link to="/swapstack">
-                      <img
-                        className="h-8 w-auto"
-                        src={SwapstackLogo}
-                        alt="Workflow"
-                      />
-                    </Link>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="mt-6 sm:mt-8">
-                    <nav>
-                      <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                        {navButtons.map(item => (
-                          <Link
-                            to={item.to}
-                            className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50"
-                            key={item.name}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </nav>
-                  </div>
-                </div>
-                <div className="py-6 px-5">
-                  <div className="mt-6">
+                    {navButtons.map(item => (
+                      <Link
+                        className="text-base font-medium text-gray-500 hover:text-gray-900"
+                        to={item.to}
+                        activeClassName="text-base font-bold text-gray-900 hover:text-gray-900"
+                        key={item.name}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </Popover.Group>
+                  {/* Sign up buttons */}
+                  <div className="flex items-center md:ml-12">
                     <a
-                      href="https://app.swapstack.co/login?key=signup"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                      href="https://app.swapstack.co/login?key=login"
+                      className="text-base font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      Sign in
+                    </a>
+                    <a
+                      href="https://app.swapstack.co/login"
+                      className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                     >
                       Sign up
                     </a>
-                    <p className="mt-6 text-center text-base font-medium text-gray-500">
-                      Existing customer?{" "}
-                      <a
-                        href="https://app.swapstack.co/login?key=login"
-                        className="text-indigo-600 hover:text-indigo-500"
-                      >
-                        Sign in
-                      </a>
-                    </p>
                   </div>
                 </div>
               </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
-    </Popover>
+            </div>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              enter="duration-200 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                static
+                className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                  <div className="pt-5 pb-6 px-5 sm:pb-8">
+                    <div className="flex items-center justify-between">
+                      <Link to="/swapstack">
+                        <img
+                          className="h-8 w-auto"
+                          src={SwapstackLogo}
+                          alt="Workflow"
+                        />
+                      </Link>
+                      <div className="-mr-2">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                          <span className="sr-only">Close menu</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    <div className="mt-6 sm:mt-8">
+                      <nav>
+                        <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
+                          {navButtons.map(item => (
+                            <Link
+                              to={item.to}
+                              className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50"
+                              key={item.name}
+                            >
+                              {item.name}
+                            </Link>
+                          ))}
+                        </div>
+                      </nav>
+                    </div>
+                  </div>
+                  <div className="py-6 px-5">
+                    <div className="mt-6">
+                      <a
+                        href="https://app.swapstack.co/login?key=signup"
+                        className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                      >
+                        Sign up
+                      </a>
+                      <p className="mt-6 text-center text-base font-medium text-gray-500">
+                        Existing customer?{" "}
+                        <a
+                          href="https://app.swapstack.co/login?key=login"
+                          className="text-indigo-600 hover:text-indigo-500"
+                        >
+                          Sign in
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>{" "}
+      {/* <Banner /> */}
+    </div>
   )
 }
---
+++ src/components/index/banner.js
@@ -0,0 +1,36 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { SpeakerphoneIcon, XIcon } from "@heroicons/react/outline"
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Banner() {
+  return (
+    <div className="bg-indigo-600">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="w-0 flex-1 flex items-center">
+            <span className="flex p-2 rounded-lg bg-indigo-800">
+              <SpeakerphoneIcon
+                className="h-6 w-6 text-white"
+                aria-hidden="true"
+              />
+            </span>
+            <p className="ml-3 font-medium text-white truncate">
+              <span className="md:hidden">No Fee November!</span>
+              Big news! Brands pay 0% fees from now until the end of November!
+              <span className="hidden md:inline"></span>
+            </p>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <Link
+              to="/how-to-reach-your-first-1000-email-subscribers"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Read the blog post!
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
