0804ce39569c73816e9b98ade5244834d79e804e
Jake <jacob.b.singer@gmail.com>
2022-01-28 18:27:51 -0500

    legal post formatting

--- src/templates/legal-post.js
+++ src/templates/legal-post.js
@@ -89,13 +89,14 @@ const LegalPostTemplate = ({ data, location }) => {
         <div className="relative px-4 sm:px-6 lg:px-8">
           <div className="text-lg max-w-prose mx-auto">
             <h1>
-              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              {/* <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
                 Legal Post
-              </span>
+              </span> */}
               <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 {post.frontmatter.title}
               </span>
             </h1>
+            <h3 className="mt-8 text-xl text-gray-500 leading-8">{`Last Updated: ${post.frontmatter.date}`}</h3>
           </div>
           <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
             <section
