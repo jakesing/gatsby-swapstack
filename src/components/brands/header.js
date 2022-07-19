20da76a227ed0f1cf65fdaf9e55da16a5df21fac
Jake <jacob.b.singer@gmail.com>
2022-07-19 15:50:29 -0400

    update calendly link on brands page

--- src/components/brands/header.js
+++ src/components/brands/header.js
@@ -22,7 +22,7 @@ export default function Header() {
             </a>
             <a
               target="_blank"
-              href="https://calendly.com/swapstack/swapstack-initial-meeting"
+              href="https://calendly.com/d/dr5-kyj-ykj/swapstack-introduction?utm_medium=website&utm_source=brands&utm_campaign=lp"
               className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Schedule a Demo!
